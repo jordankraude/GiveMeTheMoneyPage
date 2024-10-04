f5918cba26601f9bf1b7d4f9cb7bc41128035015
jordankraude <jordankraudeTP@gmail.com>
2024-10-03 20:54:23 -0600

    last little styling changes

--- donate/src/app/donate/cancel/page.tsx
+++ donate/src/app/donate/cancel/page.tsx
@@ -21,18 +21,18 @@ export default function CancelPage() {
 
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-red-50'>
-      <h1 className='text-4xl font-bold mb-4 text-red-600'>Donation Cancelled</h1>
+      <h1 className='text-4xl font-bold mb-4 text-[#3b150b]'>Donation Cancelled</h1>
       <p className='text-lg text-gray-600 mb-8'>We&apos;re sorry to see you go. If you have any questions, feel free to contact us.</p>
       <div className='flex space-x-4'>
         <button 
           onClick={() => router.push('/donate/donate-third')} // Redirect to the 3rd party donation page
-          className='bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 hover:bg-blue-700'
+          className='bg-[#5e17eb] hover:bg-white text-white hover:text-[#5e17eb] font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300'
         >
           Return to 3rd Party Donation
         </button>
         <button 
           onClick={() => router.push('/donate/donate-seamless')} // Redirect to the seamless donation page
-          className='bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 hover:bg-blue-700'
+          className='bg-[#3b150b] hover:bg-white text-white hover:text-[#3b150b] font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300'
         >
           Return to Seamless Donation
         </button>
